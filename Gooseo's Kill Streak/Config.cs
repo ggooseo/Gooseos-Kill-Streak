6cb281a2b7e5d4cc92be3919d9ff1d7a81d11dce
Gooseo <84007300+ggooseo@users.noreply.github.com>
2024-04-11 01:51:25 +0400

    aaaaaa

--- Gooseo's Kill Streak/Config.cs
+++ Gooseo's Kill Streak/Config.cs
@@ -1,12 +1,24 @@
 ﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
 
 namespace gooseoskillstreak
 {
     public class Config : IConfig
     {
-        public ushort BroadcastEveryAmountKill { get; set; } = 5;
-        public ushort BroadcastDuration { get; set; } = 3;
-        public string KillStreakMessage { get; set; } = "<color=yellow>%attacker%</color> just killed <color=blue>%victim%</color> and is now on a <color=red>%kills%</color> kill streak!";
+        public bool KillStreakSmallRewards { get; set; } = true;
+        public bool KillStreakLargeRewards { get; set; } = true;
+
+        public ushort EveryKillAmountSmallReward { get; set; } = 5;
+        public ushort EveryKillAmountBigReward { get; set; } = 10;
+
+        public List<ItemType> SmallRewardItems { get; set; } = new() { ItemType.Adrenaline, ItemType.Painkillers };
+        public List<ItemType> BigRewardItems { get; set; } = new() { ItemType.SCP500, ItemType.Coin, ItemType.Ammo556x45, ItemType.Ammo12gauge, ItemType.Ammo44cal, ItemType.Ammo762x39, ItemType.Ammo9x19 };
+
+        public ushort EveryKillAmountBroadcast { get; set; } = 5;
+        public ushort KillstreakBroadcastDuration { get; set; } = 3;
+
+        public string KillStreakBroadcastMessage { get; set; } = "<color=orange>%attacker%</color> just killed <color=orange>%victim%</color> and is now on a <color=red>%kills%</color> kill streak!";
+        public string KillStreakRewardMessage { get; set; } = "You have been <color=yellow>rewarded</color> with <color=orange>%amount%</color> items!";
 
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
--- Gooseo's Kill Streak/EventHandler.cs
+++ Gooseo's Kill Streak/EventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Exiled.API.Features;
+using Exiled.API.Features.Pickups;
 using Exiled.Events.EventArgs.Player;
 using PlayerHandler = Exiled.Events.Handlers.Player;
 
@@ -41,23 +42,54 @@ namespace gooseoskillstreak.Events
             if (attacker == null)
                 return;
 
-            if (!attacker.SessionVariables.TryGetValue("Kills", out var killsObj) || !(killsObj is int kills))
+            if (!attacker.TryGetSessionVariable("Kills", out int kills))
             {
-                attacker.SessionVariables["Kills"] = 1;
+                attacker.SessionVariables.Add("Kills", 1);
                 return;
             }
 
             kills++;
             attacker.SessionVariables["Kills"] = kills;
 
-            if (kills % config.BroadcastEveryAmountKill == 0)
+            if (kills % config.EveryKillAmountBroadcast == 0)
             {
-                string message = config.KillStreakMessage
+                string message = config.KillStreakBroadcastMessage
                     .Replace("%attacker%", attacker.DisplayNickname)
                     .Replace("%victim%", victim.DisplayNickname)
                     .Replace("%kills%", kills.ToString());
 
-                Map.Broadcast(config.BroadcastDuration, message, default, true);
+                Map.Broadcast(config.KillstreakBroadcastDuration, message, default, true);
+            }
+
+
+            if (kills % config.EveryKillAmountBigReward == 0 && config.KillStreakLargeRewards)
+            {
+                foreach (ItemType itemType in config.BigRewardItems)
+                {
+                    if (attacker.IsInventoryFull)
+                        Pickup.CreateAndSpawn(itemType, attacker.Position, attacker.Rotation);
+                    else
+                        attacker.AddItem(itemType);
+                }
+
+                string message = config.KillStreakRewardMessage
+                    .Replace("%amount%", config.BigRewardItems.Count.ToString());
+
+                attacker.Broadcast(config.KillstreakBroadcastDuration, message, default, true);
+            } else if (kills % config.EveryKillAmountSmallReward == 0 && config.KillStreakSmallRewards)
+            {
+                foreach (ItemType itemType in config.SmallRewardItems)
+                {
+                    if (attacker.IsInventoryFull)
+                        Pickup.CreateAndSpawn(itemType, attacker.Position, attacker.Rotation);
+                    else
+                        attacker.AddItem(itemType);
+                }
+
+                string message = config.KillStreakRewardMessage
+                    .Replace("%amount%", config.SmallRewardItems.Count.ToString());
+
+                attacker.Broadcast(config.KillstreakBroadcastDuration, message, default, true);
             }
         }
     }
